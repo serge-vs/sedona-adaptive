3f93e2cd94ef880ae168fb354e7f9e12a994c575
serge-vs <dvhorizon@gmail.com>
2021-06-30 22:41:03 +1000

    Many changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,186 @@
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const sourcemap = require('gulp-sourcemaps');
+// const sourcemap = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const sync = require('browser-sync').create();
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
 
 // Styles
 
 const styles = () => gulp.src('source/sass/style.scss')
   .pipe(plumber())
-  .pipe(sourcemap.init())
+  // .pipe(sourcemap.init())
   .pipe(sass({ outputStyle: 'expanded' }))
   .pipe(postcss([
     autoprefixer(),
   ]))
-  .pipe(sourcemap.write('.'))
-  .pipe(gulp.dest('source/css'))
-  .pipe(sync.stream());
+  // .pipe(gulp.dest('build/css'))
+  // .pipe(browserSync.stream())
+  .pipe(csso({ restructure: false }))
+  .pipe(rename('styles.min.css'))
+  // .pipe(sourcemap.write('.'))
+  .pipe(gulp.dest('build/css'))
+  .pipe(browserSync.stream());
 
 exports.styles = styles;
+
+// Delete files from build
+
+const clean = () => del('build');
+
+exports.clean = clean;
+
+// Copy files from source to build
+
+const copy = () => gulp.src([
+  'source/fonts/**/*.{woff,woff2}',
+  'source/img/**',
+  'source/video/**',
+  'source/*.ico',
+  'source/*.webmanifest',
+  // 'source/js/**',
+], {
+  base: 'source',
+})
+  .pipe(gulp.dest('build'));
+
+exports.copy = copy;
+
+// JS min and copy to build
+
+const jsmin = () => gulp.src('source/js/*.js')
+  .pipe(uglify())
+  .pipe(rename((path) => {
+    const thisPath = path;
+    thisPath.basename += '.min';
+  }))
+  .pipe(gulp.dest('build/js'));
+
+exports.jsmin = jsmin;
+
+// HTML min and copy to build
+
+const html = () => gulp.src('source/*.html')
+  .pipe(htmlmin({
+    collapseWhitespace: true,
+  }))
+  .pipe(gulp.dest('build'));
+
+exports.html = html;
+
+// Images
+
+const images = () => gulp.src('build/img/**/*.{jpg,png,svg}')
+  .pipe(imagemin([
+    imagemin.optipng({ optimizationLevel: 3 }),
+    imagemin.mozjpeg({ progressive: true }),
+    imagemin.svgo({
+      plugins: [
+        { removeViewBox: false },
+        { removeUselessStrokeAndFill: false },
+        { cleanupIDs: false },
+      ],
+    }),
+  ]))
+  .pipe(gulp.dest('build/img'));
+
+exports.images = images;
+
+// WebP
+
+const createWebp = () => gulp.src(['build/img/**/*.{png,jpg}', '!build/img/favicons/**/*'])
+  .pipe(webp({ quality: 90 }))
+  .pipe(gulp.dest('build/img'));
+
+exports.webp = createWebp;
+
+// SVG Sprite
+
+const sprite = () => gulp.src('source/img/**/icon-*.svg')
+  .pipe(svgstore({
+    inlineSvg: true,
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/img'));
+
+exports.sprite = sprite;
+
+// Reload
+
+const reload = (done) => {
+  browserSync.reload();
+  done();
+};
+
+exports.reload = reload;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('source/js/*.js', gulp.series('jsmin', 'reload'));
+  gulp.watch('source/*.html', gulp.series('html', 'reload'));
+};
+
+exports.watcher = watcher;
 
 // Server
 
 const server = (done) => {
-  sync.init({
+  browserSync.init({
     server: {
-      baseDir: 'source',
+      baseDir: 'build',
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
+
+  watcher();
 };
 
 exports.server = server;
 
-// Watcher
+// Build
 
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
-  gulp.watch('source/*.html').on('change', sync.reload);
-};
+const build = gulp.series(
+  clean,
+  copy,
+  images,
+  gulp.parallel(
+    createWebp,
+    styles,
+    sprite,
+    html,
+    jsmin,
+  ),
+);
+
+exports.build = build;
+
+// Default (npm run gulp)
 
 exports.default = gulp.series(
-  styles, server, watcher,
+  gulp.parallel(
+    styles,
+    html,
+    jsmin,
+  ),
+  gulp.series(
+    server,
+  ),
 );
+
+// exports.default = gulp.series(
+//   styles, server, watcher,
+// );
---
+++ source/js/menu.js
@@ -0,0 +1,90 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerButton = document.querySelector('.burger-button');
+  const listToggle = document.querySelector('.site-list');
+  const body = document.querySelector('body');
+
+  // Проверка: является ли устройство мобильным (touchscreen), для отображения стрелок в меню
+  const isMobile = navigator.userAgentData.mobile;
+
+  const isMobileOld = {
+    Android() {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry() {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera() {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows() {
+      return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any() {
+      return (isMobileOld.Android() || isMobileOld.BlackBerry() || isMobileOld.iOS()
+      || isMobileOld.Opera() || isMobileOld.Windows());
+    },
+  };
+
+  if (isMobile || isMobileOld.any()) {
+    body.classList.add('touch');
+
+    let arrowWrapper = document.querySelector('.js-arrow-wrapper');
+    let arrows = document.querySelectorAll('.js-arrow');
+
+    arrows.forEach((arrow) => {
+      let thisArrow = arrow;
+      let thisLink = arrow.previousElementSibling;
+      let subMenu = arrowWrapper.nextElementSibling;
+
+      thisLink.classList.add('link-parent');
+      arrow.addEventListener('click', () => {
+        subMenu.classList.toggle('open-menu');
+        thisArrow.classList.toggle('active-arrow');
+      });
+    });
+  } else { body.classList.add('mouse'); }
+
+  /*
+  Если JS не загрузится, класс nojs скрывает кнопку открыть/закрыть меню,
+  меню отображается открытым. Класс site-list--closed скрывает меню.
+  */
+
+  burgerButton.classList.remove('burger-button--nojs');
+  burgerButton.classList.remove('burger-button--close');
+  listToggle.classList.add('site-list--closed');
+
+  burgerButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    // Переключение кнопки и списка меню без анимации
+    // burgerButton.classList.toggle('burger-button--close');
+    // listToggle.classList.toggle('site-list--closed');
+
+    // Переключение кнопки и списка меню c анимацией
+    if (listToggle.classList.contains('site-list--closed')) {
+      burgerButton.classList.add('burger-button--close');
+      listToggle.classList.remove('site-list--closed');
+      // listToggle.style.height = 'auto';
+
+      // let height = `${listToggle.clientHeight}px`;
+
+      // listToggle.style.height = '0px';
+
+      // setTimeout(() => {
+      //   listToggle.style.height = height;
+      // }, 20);
+    } else {
+      burgerButton.classList.remove('burger-button--close');
+      // listToggle.style.height = '0px';
+
+      // listToggle.addEventListener('transitionend', () => {
+      listToggle.classList.add('site-list--closed');
+      // }, {
+      //   once: true,
+      // });
+    }
+  });
+});
